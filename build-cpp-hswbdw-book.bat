c3c73c41b6070f2324f07d0d8f73f5d1a9dc8690
Matt Devo <matt.devillier@gmail.com>
2016-12-13 19:42:02 -0600

    Add batch files for building

---
+++ build-cpp-hswbdw-book.bat
@@ -0,0 +1,6 @@
+set "var=%cd%"
+rmdir /s /q .\Build
+copy ..\_GOP\vbt-hswbdw-book.bin CorebootPayloadPkg\vbt.bin
+copy ..\_GOP\IntelBdwGopDriver_1033.efi CorebootPayloadPkg\IntelGopDriver.efi
+build -a IA32 -a X64 -p CorebootPayloadPkg\CorebootPayloadPkgIa32X64.dsc -b RELEASE -t VS2012x86 -n 4
+copy .\Build\CorebootPayloadPkgX64\RELEASE_VS2012x86\FV\UEFIPAYLOAD.fd z:\firmware\tianocore\UEFIPAYLOAD-hswbdw-book.fd
---
+++ build-cpp.bat
@@ -0,0 +1,4 @@
+call build-cpp-hswbdw-book.bat
+call build-cpp-hswbdw-box.bat
+call build-cpp-ivb-book.bat
+call build-cpp-snb-book.bat
